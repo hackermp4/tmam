a129c1a84d996a63b364095c85e4938388dcc241
Intishar Ishmam <intishar.ishmam@jobike.io>
2020-07-10 23:49:13 +0600

    I working 4

--- admin/app/base_url.php
+++ admin/app/base_url.php
@@ -1,5 +1,5 @@
 <?php
-$subdir = 'tproject/';
+$subdir = 'tmam/';
 $base_url = isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == 'on' ? 'https://' . $_SERVER["HTTP_HOST"] . '/' . $subdir : 'http://' . $_SERVER["HTTP_HOST"] . '/' .$subdir;
 
 define('SITE_URL', $base_url );
